1c24d7da6278616cdb61243085978d7059b5f33e
Justin Wilkinson <justin_wilkinson@talktalk.net>
2020-04-29 12:07:26 +0100

    Get synchronized drag working on screens of same size

--- SixNimmt/Client/wwwroot/js/sixnimmt.js
+++ SixNimmt/Client/wwwroot/js/sixnimmt.js
@@ -1,3 +1,7 @@
 ﻿window.sixnimmt = {
-
+    getElementCoordinates: function (element) {
+        let bounds = element.getBoundingClientRect();
+        let parentStyles = window.getComputedStyle(element.parentElement);
+        return { X: bounds.left - parseFloat(parentStyles.paddingLeft), Y: bounds.top - parseFloat(parentStyles.paddingTop) }
+    }
 }
