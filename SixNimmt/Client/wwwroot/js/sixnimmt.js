4e9656e97f227777c38a0a35fd52f45a88f69765
Justin <36571008+JustinWilkinson@users.noreply.github.com>
2020-04-29 15:54:29 +0100

    Sync drag (#3)
    * Get synchronized drag working on screens of same size
    
    * Get synchronized drag working on screens of different sizes
    
    * Tidy up drag and drop
    
    * Extract repetitive card HTML into component

--- SixNimmt/Client/wwwroot/js/sixnimmt.js
+++ SixNimmt/Client/wwwroot/js/sixnimmt.js
@@ -1,3 +1,18 @@
 ﻿window.sixnimmt = {
-
+    getElementCoordinates: function (element) {
+        let bounds = element.getBoundingClientRect();
+        let parentStyles = window.getComputedStyle(element.parentElement);
+        return { X: bounds.left - parseFloat(parentStyles.paddingLeft), Y: bounds.top - parseFloat(parentStyles.paddingTop) }
+    },
+    getScreenSize: function () {
+        return { X: window.innerWidth, Y: window.innerHeight }
+    },
+    getCardSize: function () {
+        let cards = document.getElementsByClassName('card');
+        if (cards.length > 0) {
+            let styles = window.getComputedStyle(cards[0]);
+            return { X: parseFloat(styles.width), Y: parseFloat(styles.height) };
+        }
+        return null;
+    }
 }
